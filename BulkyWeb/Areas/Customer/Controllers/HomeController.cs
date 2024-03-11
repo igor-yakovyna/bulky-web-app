5cc33613e52baa5e6d8aadba0e48bc52c244f74c
Ihor Yakovyna <iyakov@softserveinc.com>
2024-03-11 20:27:44 +0200

    Finishing section 7

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BulkyBook.DataAccess.Repository.Interfaces;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,27 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _unitOfWork.ProductRepository.GetAll(includeProperties: "Category");
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _unitOfWork.ProductRepository.GetFirstOrDefault(p => p.Id == id, includeProperties: "Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()
